95f8f4e840f81a5b99b0cb523177c4cfe6c8cfc3
ofca <sven.dupli@gmail.com>
2016-11-27 20:37:22 +0100

    Set up first tests

---
+++ spec/features/navigation.rb
@@ -0,0 +1,17 @@
+
+require 'rails_helper'
+
+RSpec.feature "navigation" do
+
+  it "should navigate to about page from home page" do
+    visit root_path
+    click_on "engage!"
+    expect(page).to have_current_path about_path
+  end
+
+  it "should navigate to home page from about page" do
+    visit about_path
+    click_on "logo"
+    expect(page).to have_current_path root_path
+  end
+end
