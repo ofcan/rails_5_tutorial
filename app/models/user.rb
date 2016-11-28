629821331c9bc414bc94b073b54c206f47a199dd
sven.dupli <sven.dupli@gmail.com>
2016-11-28 06:00:46 -0500

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
-
-  validates :name, presence: true
+  
+  before_save { self.email = email.downcase }
+  has_secure_password
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :name, presence: true  
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }  
+  validates :password, presence: true,
+                       length: { minimum: 6 }
 
 end
--- spec/features/users.rb
+++ spec/features/users.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.feature "users" do
 
   background do
-    @user = User.new(name: 'user', email: 'user@example.com')
+    @user = User.new(name: 'user', email: 'user@example.com', password: 'secret123', password_confirmation: 'secret123')
   end
 
   it 'should create a new user' do
@@ -21,12 +21,29 @@ RSpec.feature "users" do
     expect(@user).to_not be_valid
   end
   
+  it 'should not create a new user without a password' do
+    user = User.new(name: 'user', email: 'user@example.com', password: '', password_confirmation: 'secret123')
+    expect(user).to_not be_valid
+  end
+  
+  it 'should not create user with wrong password_confirmation' do
+    @user.password_confirmation = '123'
+    expect(@user).to_not be_valid
+  end
+  
   it 'should accept only valid email addresses' do
     addresses = %w[bubba@lula sven@gmail..com]
     addresses.each do |address|
       @user.email = address
       expect(@user).to_not be_valid
     end
+  end
+  
+  it 'should not create duplicate users' do
+    @user.save
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    expect(duplicate_user).to_not be_valid
   end
 
  # it "should create a new user" do
