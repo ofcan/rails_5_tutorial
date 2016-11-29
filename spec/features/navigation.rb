9743d94eb6de4911046877a26507a1de288c9e72
ofca <sven.dupli@gmail.com>
2016-11-29 19:33:06 +0100

    Improve the look

--- spec/features/navigation.rb
+++ spec/features/navigation.rb
@@ -5,7 +5,7 @@ RSpec.feature "navigation" do
 
   it "should navigate to about page from home page" do
     visit root_path
-    click_on "engage!"
+    click_on "about"
     expect(page).to have_current_path about_path
   end
 
@@ -13,6 +13,24 @@ RSpec.feature "navigation" do
     visit about_path
     click_on "logo"
     expect(page).to have_current_path root_path
+  end
+
+  it 'should navigate to new user page from home' do
+    visit root_path
+    click_on "sign_up"
+    expect(page).to have_current_path new_user_path
+  end
+
+  it 'should navigate to users page from home' do
+    visit root_path
+    click_on "users"
+    expect(page).to have_current_path users_path
+  end
+
+  it 'should navigate to user page from user index' do
+    user = User.create(name: 'sven', email: 'sven@example.com', password: 'secret123', password_confirmation: 'secret123')
+    visit users_path
+    click_link "#{user.name}"
   end
 
 end
