958f84a421edf82413190110830030e0eeefbb83
ofca <sven.dupli@gmail.com>
2016-12-03 18:21:27 +0100

    Begun user-microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,10 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = "this is a micropost"
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
