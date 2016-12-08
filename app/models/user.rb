897a21793845588e3b04186e4c200a3dd69687c3
ofca <sven.dupli@gmail.com>
2016-12-08 20:46:17 +0100

    User feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,7 +105,14 @@ class User < ApplicationRecord
   def followed_by?(another_user)
     followers.include?(another_user)
   end
-    
+
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
   private
     
     # Downcases an email
